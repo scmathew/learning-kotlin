99f065fcd0add8cf87fdd552e02808f82e75da85
Santosh Mathew <smathew@captechventures.com>
2021-01-26 14:37:27 -0500

    Data classes!

--- src/com/demo/learning/kotlin/Customer.kt
+++ src/com/demo/learning/kotlin/Customer.kt
@@ -4,18 +4,40 @@ package com.demo.learning.kotlin
 fun main() {
     val customer = Customer("Santosh", "1 best street", 56)
     customer.age = 99
-    println("${customer.name} is ${customer.age} years old")
+    println("${customer.name} is ${customer.age} years old\n")
 
     val customer2 = Customer("John", 11)
-    println("${customer2.name} is ${customer2.age} years old")
+    println("${customer2.name} is ${customer2.age} years old\n")
+
+
+    val customer3 = AnotherAlternativeCustomer("FirstName", 20)
+    customer3.approved = true
+    println("${customer3.name} is ${customer3.age} years old")
+    println("Next year ${customer3.name} is ${customer3.nextAge} years old\n")
+
+    val customer4 = AnotherAlternativeCustomer.getInstance()
+    println(customer4)
+
+    val customer5 = Customer("New Name", 56)
+    println(customer5.toString() + "\n")
+
+
+    val customer6 = customer5.copy(name = "New Name Twin")
+    println(customer6.toString() + "\n")
+
+    // Destructuring
+    val (name, address, age) = customer6
+    println(name)
+
+
 }
 
 
 
-// Classes, fields are public by default
-// The parameters are fields
-// In the parentheses in the primary constructor
-class Customer (val name: String, val address: String, var age: Int) {
+// Data class get built in toString, hashCode, and equals method
+// also get the copy function
+// also get destructuring
+data class Customer (val name: String, val address: String, var age: Int) {
 
     // This is a secondary constructor
     // Has to call the primary constructor
@@ -28,6 +50,9 @@ class Customer (val name: String, val address: String, var age: Int) {
     }
 }
 
+// Classes, fields are public by default
+// The parameters are fields
+// In the parentheses in the primary constructor
 class AlternativeConstructor (val name: String, var age: Int) {
     // Can have combo of fields in primary constructor and code block
     var address: String
@@ -42,7 +67,39 @@ class AlternativeConstructor (val name: String, var age: Int) {
     constructor(name: String, address: String, age: Int): this(name, age) {
         this.address = address
     }
-
 }
+
+class AnotherAlternativeCustomer (val name: String, var age: Int, val address: String = "") {
+    var approved: Boolean = false
+
+    // custom setter
+    // Field is approved because its right under it ??
+    set(value) {
+        if (age >= 21) {
+            field = value
+        } else {
+            println("You can't approve a customer under 21 years of age.")
+        }
+    }
+
+    // Custom get, for a field that doesn't exist
+    val nextAge: Int
+    get() = age + 1
+
+    fun upperCaseName() =
+            name.toUpperCase()
+
+    // override toString func
+    override fun toString(): String {
+        return "$name $address $age"
+    }
+
+    // Everything in this block is static
+    // This is for when you want static functions inside a class
+    companion object {
+        fun getInstance() = AnotherAlternativeCustomer("Micky", 22, "Some Address")
+    }
+}
+
 
 class Book (val id: Long, val title: String = "Default Title")
