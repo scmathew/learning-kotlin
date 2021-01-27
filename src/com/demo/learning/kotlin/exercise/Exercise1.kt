a287207109c23579b5d56b998092ab670e8303c8
Santosh Mathew <smathew@captechventures.com>
2021-01-27 11:26:34 -0500

    Elvis operator!

--- src/com/demo/learning/kotlin/exercise/Exercise1.kt
+++ src/com/demo/learning/kotlin/exercise/Exercise1.kt
@@ -10,19 +10,27 @@ data class KotlinPerson(
         val dateOfBirth: Calendar?
 ) {
 
+    val safeAge: Int
+        get() {
+            // Elvis operator, similar to ternary
+            // return age if not null, else return -1
+            return age ?: -1
+        }
 
-
-    val age: Int
+    val age: Int?
         get() = getAge(dateOfBirth)
 
     companion object {
-        fun getAge(dateOfBirth: Calendar?): Int {
-            if (dateOfBirth == null) return -1
-            
+        fun getAge(dateOfBirth: Calendar?): Int? {
+            if (dateOfBirth == null) return null
+
             val today: Calendar = GregorianCalendar()
             val years: Int = today.get(Calendar.YEAR) - dateOfBirth.get(Calendar.YEAR)
 
             val flag = dateOfBirth.get(Calendar.DAY_OF_YEAR) >= today.get(Calendar.YEAR)
+
+            // if in ternary-ish form
+            // no actual ternary operator in Kotlin
             return if(flag) years - 1 else years
 
             // Long version
@@ -46,6 +54,6 @@ fun main() {
     println("$jane's age is ${jane.age}")
     println("The age of someone born on 3rd May 1988 is ${KotlinPerson.getAge(GregorianCalendar(1988, 5, 3))}")
 
-    val olderPerson = if (john.age > jane.age) john else jane
+    val olderPerson = if (john.safeAge > jane.safeAge) john else jane
     println("The older person is $olderPerson")
 }
